c08c19e8fd74d048ed7592eedac42a3b06b82038
Mengjuei Hsieh <mhsieh@pacificbiosciences.com>
2017-08-01 17:50:09 -0700

    see if we can do it with anaconda

--- bamboo_build.sh
+++ bamboo_build.sh
@@ -3,55 +3,38 @@ type module >& /dev/null || . /mnt/software/Modules/current/init/bash
 module load git/2.8.3
 module load gcc/4.9.2
 module load ccache/3.2.3
+export CCACHE_DIR=/mnt/secondary/Share/tmp/bamboo.mobs.ccachedir
+module load boost/1.60
+if [[ $BOOST_ROOT =~ /include ]]; then
+  set -x
+  BOOST_ROOT=$(dirname $BOOST_ROOT)
+  set +x
+fi
+module load anaconda/4.x.x
 
-mkdir -p prebuilts/libbzip2-1.0.6
-curl -sL http://ossnexus/repository/unsupported/pitchfork/gcc-4.9.2/libbzip2-1.0.6.tgz \
-| tar zxf - -C prebuilts/libbzip2-1.0.6
 if [ ! -e .distfiles/gtest/release-1.7.0.tar.gz ]; then
   mkdir -p .distfiles/gtest
-  curl -sL http://ossnexus/repository/unsupported/distfiles/googletest/release-1.7.0.tar.gz \
+  curl -sL http://nexus/repository/unsupported/distfiles/googletest/release-1.7.0.tar.gz \
     -o .distfiles/gtest/release-1.7.0.tar.gz
 fi
+tar zxf .distfiles/gtest/release-1.7.0.tar.gz
+ln -sfn googletest-release-1.7.0 gtest
 
-cat > pitchfork/settings.mk << EOF
-CCACHE_DIR            = /mnt/secondary/Share/tmp/bamboo.mobs.ccachedir
-DISTFILES             = $PWD/.distfiles
-# from Herb
-HAVE_OPENSSL      = /mnt/software/o/openssl/1.0.2a
-HAVE_PYTHON       = /mnt/software/p/python/2.7.9/bin/python
-HAVE_BOOST        = /mnt/software/b/boost/1.58.0
-HAVE_ZLIB         = /mnt/software/z/zlib/1.2.8
-HAVE_SAMTOOLS     = /mnt/software/s/samtools/1.3.1mobs
-HAVE_NCURSES      = /mnt/software/n/ncurses/5.9
-# from MJ
-HAVE_HDF5         = /mnt/software/a/anaconda2/4.2.0
-HAVE_OPENBLAS     = /mnt/software/o/openblas/0.2.14
-HAVE_CMAKE        = /mnt/software/c/cmake/3.2.2/bin/cmake
-HAVE_LIBBZIP2     = $PWD/prebuilts/libbzip2-1.0.6
-#
-htslib_REPO           = $PWD/htslib
-pbbam_REPO            = $PWD/pbbam
-blasr_REPO            = $PWD/blasr
-blasr_libcpp_REPO     = $PWD/blasr_libcpp
-pbcore_REPO           = $PWD/pbcore
-pbalign_REPO          = $PWD/pbalign
-pbcopper_REPO         = $PWD/pbcopper
-seqan_REPO            = $PWD/seqan
-unanimity_REPO        = $PWD/unanimity
-ConsensusCore_REPO    = $PWD/ConsensusCore
-GenomicConsensus_REPO = $PWD/GenomicConsensus
-pbcommand_REPO        = $PWD/pbcommand
-pbcoretools_REPO      = $PWD/pbcoretools
-pypeFLOW_REPO         = $PWD/pypeFLOW
-daligner_REPO         = $PWD/daligner
-dextractor_REPO       = $PWD/dextractor
-dmasker_REPO          = $PWD/dmasker
-dazzdb_REPO           = $PWD/dazzdb
-pbdagcon_REPO         = $PWD/pbdagcon
-bam2fastx_REPO        = $PWD/bam2fastx
-falcon_polish_REPO    = $PWD/falcon_polish
-falcon_kit_REPO       = $PWD/falcon
-EOF
-cd pitchfork
-echo y|make _startover
-make -j10 falcon_polish nose
+rm -rf deployment && mkdir deployment
+export PYTHONUSERBASE=$PWD/deployment
+
+GenomicConsensus_VERSION=$(cd GenomicConsensus && python setup.py --version 2> /dev/null)
+ConsensusCore_VERSION=$(cd ConsensusCore && python setup.py --version 2> /dev/null)
+ConsensusCore2_VERSION=$(cd unanimity && python setup.py --version 2> /dev/null)
+
+pip install --user --no-index http://nexus/repository/unsupported/pitchfork/gcc-4.9.2/pythonpkgs/networkx-1.10-py2.py3-none-any.whl
+pip install --user --no-index ./falcon
+pip install --user --no-index http://nexus/repository/unsupported/pitchfork/gcc-4.9.2/pythonpkgs/avro-1.7.7-cp27-none-any.whl
+pip install --user --no-index http://nexus/repository/unsupported/pitchfork/gcc-4.9.2/pythonpkgs/iso8601-0.1.12-py2.py3-none-any.whl
+pip install --user --no-index ./pbcommand
+pip install --user --no-index http://nexus/repository/unsupported/gcc-4.9.2/pythonpkgs/ConsensusCore-${ConsensusCore_VERSION}-cp27-cp27mu-linux_x86_64.whl
+pip install --user --no-index http://nexus/repository/unsupported/gcc-4.9.2/pythonpkgs/ConsensusCore2-${ConsensusCore2_VERSION}-cp27-cp27mu-linux_x86_64.whl
+pip install --user --no-index http://nexus/repository/unsupported/pitchfork/gcc-4.9.2/pythonpkgs/pysam-0.9.1.4-cp27-cp27mu-linux_x86_64.whl
+pip install --user --no-index ./pbcore
+pip install --user --no-index http://nexus/repository/unsupported/gcc-4.9.2/pythonpkgs/GenomicConsensus-${GenomicConsensus_VERSION}-cp27-cp27mu-linux_x86_64.whl
+pip install --user --no-index ./falcon_polish
